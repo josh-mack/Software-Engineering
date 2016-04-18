eaea191dc63c29e4c63a0eab4484437629915d49
super <super@128.4.186.71>
2016-04-18 11:17:19 -0400

    serialized Environment

--- src/Estuary/Environment.java
+++ src/Estuary/Environment.java
@@ -1,6 +1,12 @@
 package Estuary;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class Environment {
+public class Environment implements Serializable{
 	private Species[] animals;
 	private Character[] characters;
 	private eChar[][] board;
@@ -78,6 +84,49 @@ public class Environment {
 	
 	public void calcMoney() {
 		
+	}
+	
+	/**
+	 * Method to serialize Environment, which takes care of all the
+	 * game logic, containing the serialized version of the objects.
+	 * @param obj
+	 * @param fileName
+	 * 
+	 * @throws IOException
+	 */
+	public static void serializable(Object obj, String fileName) {
+		try {
+			FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(obj);
+			fos.close();
+		} catch(IOException e) {
+			System.out.println("Read error: " + e.getMessage());
+		}
+	}
+	
+	/**
+	 * Method to read game state.
+	 * @param fileName
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @return Environment object
+	 */
+	public static Object deserialize(String fileName) {
+		Environment obj = null ;
+		try {	
+			FileInputStream fis = new FileInputStream(fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			obj = (Environment)ois.readObject();
+			ois.close();
+		}
+		catch(IOException e) {
+			System.out.println("Read Error: " + e.getMessage());
+		}
+		catch (ClassNotFoundException e){
+			System.out.println("Read Error: " + e.getMessage());
+		}
+		return obj;
 	}
 	
 }
