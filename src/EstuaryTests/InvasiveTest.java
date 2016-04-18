c8c049cad8f794059f1117a9c46bf6976c750c99
super <super@128.4.186.71>
2016-04-18 12:26:16 -0400

    Invasive test passed

--- src/EstuaryTests/InvasiveTest.java
+++ src/EstuaryTests/InvasiveTest.java
@@ -10,8 +10,9 @@ public class InvasiveTest {
 	@Test
 	public void test() {
 		MittenCrab crab1 = new MittenCrab(5, 10, 10, 1, 150);
-		
-		fail("Not yet implemented");
+		crab1.setAmount(3);
+		crab1.setXCoord(15);
+		assert(crab1.getAmount() == 3 && crab1.getXCoord() == 15);
 	}
 
 }
