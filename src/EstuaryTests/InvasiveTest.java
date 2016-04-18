0c2d743980797fcbc3eb207161bf1ed01a5465c4
super <super@128.4.186.71>
2016-04-18 12:10:25 -0400

    invasive test incomplete

---
+++ src/EstuaryTests/InvasiveTest.java
@@ -0,0 +1,17 @@
+package EstuaryTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import Estuary.*;
+
+public class InvasiveTest {
+
+	@Test
+	public void test() {
+		MittenCrab crab1 = new MittenCrab(5, 10, 10, 1, 150);
+		
+		fail("Not yet implemented");
+	}
+
+}
