6d74d7310c16609e0bf0dcf25b50fdc8e40f4b59
William Bartlett <bbartlett12@gmail.com>
2016-04-25 13:14:38 -0400

    Finished DNERR

--- src/Estuary/DNERR.java
+++ src/Estuary/DNERR.java
@@ -1,31 +1,54 @@
 package Estuary;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 
-public class DNERR extends Component
+public class DNERR extends JComponent
 {
 	short level = 1;
+	ImageIcon building;
+	
+	public DNERR()
+	{
 		
+		setLayout(new BorderLayout());
+		building = new ImageIcon("imgs/level1.png");
+		JLabel label = new JLabel(building);
+		label.setBounds(0, 0, building.getIconWidth(), building.getIconHeight());
+		setBounds(0,0,building.getIconWidth(), building.getIconHeight());
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setVerticalAlignment(JLabel.CENTER);
+		add(label);
+		
+		JButton upgrade = new JButton("Upgrade");
+		upgrade.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				upgrade();
+			}
+		});
+	}
+		
+
 	void upgrade()
 	{
 		switch(level)
 		{
 		case 2:
-			//Insert new image for DNERR
+			Environment.money -= 200;
+			building = new ImageIcon("img/level2");
 			break;
 		case 3:
-			//Insert level three DNERR
+			Environment.money -= 500;
+			building = new ImageIcon("imgs/level3");
 			break;
 		default:
-			break;
+			building = new ImageIcon("img.level2");
 		}
 	}
 	
