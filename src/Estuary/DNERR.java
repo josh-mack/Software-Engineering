6638d8bd5c29c77a3b84b9b8d614a145c67ee244
super <super@128.4.188.226>
2016-04-25 13:19:53 -0400

    Serialize DNERR, mouseRelease repaint, changed tick timer back, menu repaint

--- src/Estuary/DNERR.java
+++ src/Estuary/DNERR.java
@@ -1,7 +1,17 @@
 package Estuary;
 
-public class DNERR 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class DNERR implements Serializable
 {
+
+	private static final long serialVersionUID = 600;
+	
 	short level = 1;
 	
 	void upgrade()
@@ -19,5 +29,45 @@ public class DNERR
 		}
 	}
 	
+	/**
+	 * Method to serialize the DNERR object.
+	 * @param obj
+	 * @param fileName
+	 * 
+	 * @throws IOException
+	 */
+	public static void serializable(Object obj, String fileName) {
+		try {
+			FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(obj);
+			fos.close();
+		} catch(IOException e) {
+			System.out.println("Read error: " + e.getMessage());
+		}
+	}
 	
+	/**
+	 * Method to read game state.
+	 * @param fileName
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @return DNERR object
+	 */
+	public static Object deserialize(String fileName) {
+		DNERR obj = null ;
+		try {	
+			FileInputStream fis = new FileInputStream(fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			obj = (DNERR)ois.readObject();
+			ois.close();
+		}
+		catch(IOException e) {
+			System.out.println("Read Error: " + e.getMessage());
+		}
+		catch (ClassNotFoundException e){
+			System.out.println("Read Error: " + e.getMessage());
+		}
+		return obj;
+	}
 }
--- src/Estuary/DragComponent.java
+++ src/Estuary/DragComponent.java
@@ -68,7 +68,8 @@ public class DragComponent extends JComponent {
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+				repaint();
+				revalidate();
 			}
 	
 	
