007f864e78c38766bc2b383f40a8d184c54fa72f
super <super@128.4.188.226>
2016-04-25 14:25:29 -0400

    added eChar test

---
+++ src/EstuaryTests/eCharTest.java
@@ -0,0 +1,18 @@
+package EstuaryTests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import Estuary.*;
+
+public class eCharTest {
+
+	@Test
+	public void eChars() {
+		eChar.BAMBOO.setXLoc(10);
+		eChar.BAMBOO.setYLoc(10);
+		
+		assertEquals(eChar.BAMBOO.getXLoc(), 10);
+		assertEquals(eChar.BAMBOO.getYLoc(), 10);
+	}
+
+}
