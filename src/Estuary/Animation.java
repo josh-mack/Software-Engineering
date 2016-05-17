81d111df34cc96a158557c581e4b06ac54f26f6e
super <super@128.4.190.37>
2016-05-17 18:27:23 -0400

    deleted second package statement

--- src/Estuary/Animation.java
+++ src/Estuary/Animation.java
@@ -1,7 +1,6 @@
-package Estuary;
 //T Harvey
 //based loosely on http://www.java2s.com/Code/JavaAPI/java.awt/GraphicsdrawImageImageimgintxintyImageObserverob.htm
- package Estuary;
+package Estuary;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
